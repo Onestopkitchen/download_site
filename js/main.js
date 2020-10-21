f9b68c3b1cf906581779e096203b4462f9aa5c32
Pranjal Das <pranjaldas525@gmail.com>
2020-10-21 07:03:12 +0530

    swiper added

--- js/main.js
+++ js/main.js
@@ -29,6 +29,20 @@ function getResolution() {
 $('.carousel').carousel({
   interval: 2000 
 });
+//To swipe support in mobile
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
 
 
